7514be0c469ddbed654ae3a1fb69d919f06d97fb
AleenaKhan10 <aleenakhanraees40@gmail.com>
2023-11-18 22:30:30 +0500

    lec 33 made a post request

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,11 @@ export function GET(request: NextRequest) {
         {id : 2, name : 'Hassan'},
     ])
 }
+
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    if (!body.name){
+        return NextResponse.json({error : 'Name is required'}, {status: 401})
+    }
+    return NextResponse.json(body, {status:201})
+}

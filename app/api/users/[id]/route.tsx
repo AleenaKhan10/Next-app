7355ee2bf6baf27c77543c94285cefd75fc27e5a
AleenaKhan10 <aleenakhanraees40@gmail.com>
2023-11-18 22:57:51 +0500

    lec 35 delete request

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -9,13 +9,22 @@ export function GET(request: NextRequest, {params} : {params : {id : number}}) {
 
 
 
-export async function PUT(request: NextRequest) {
+export async function PUT(request: NextRequest, {params} : {params : {id : number}}) {
     const body = await request.json()
     if (!body.name){
         return NextResponse.json({error : 'Name is required'}, {status: 400})
     }
-    if (body.id > 10){
+    if (params.id > 10){
         return NextResponse.json({error : 'User is not found'}, {status: 404})
     }
     return NextResponse.json({name :body.name}, {status:201})
+}
+
+
+export function DELETE(request: NextRequest, {params} : {params : {id : number}}) {
+
+    if (params.id > 10){
+        return NextResponse.json({error : 'User is not found'}, {status: 404})
+    }
+    return NextResponse.json({})
 }
